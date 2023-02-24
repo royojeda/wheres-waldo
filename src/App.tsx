eaa9eed47655736e573734ee0e2a9e74549344d6
Dennis Ojeda <royojeda1@gmail.com>
2023-02-24 17:01:35 +0800

    feat: determine click location on image
    The x and y coordinates are relative to the image position and size.
    Top-left corner location is [0, 0]. Bottom-right corner is [1, 1].

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,21 @@
+/* eslint-disable jsx-a11y/no-noninteractive-element-interactions */
+
 import image from "./images/29ya069ug2f61.jpg";
 
 export default function App() {
+  const handleMouseDown: React.MouseEventHandler = (event) => {
+    if (event.target === event.currentTarget && event.button === 0) {
+      const { offsetX, offsetY } = event.nativeEvent;
+      console.clear();
+      console.log(
+        "X:",
+        offsetX / event.currentTarget.clientWidth,
+        "Y:",
+        offsetY / event.currentTarget.clientHeight
+      );
+    }
+  };
+
   return (
     <div>
       <div className="select-none bg-neutral-800 p-4 sm:p-20">
@@ -8,6 +23,7 @@ export default function App() {
           src={image}
           alt=""
           draggable="false"
+          onMouseDown={handleMouseDown}
           className="mx-auto shadow-lg shadow-neutral-900"
         />
       </div>
