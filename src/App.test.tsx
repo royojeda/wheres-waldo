5ed0f0096b440b90803d658914b44c5da897c450
Dennis Ojeda <royojeda1@gmail.com>
2023-02-24 20:19:36 +0800

    test: expect to display artwork credits

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,9 @@
 import { render, screen } from "@testing-library/react";
 import App from "./App";
 
-test("renders learn react link", () => {
+test("displays artwork credits", () => {
   render(<App />);
 
-  const element = screen.getByText(/Hello, world!/);
+  const element = screen.getByText(/A\.D\. 2\.222.*Egor Klyuchnyk/);
   expect(element).toBeInTheDocument();
 });
