31406e9926930303f6b11f9e3accb723603eabe4
Dennis Ojeda <royojeda1@gmail.com>
2023-02-24 18:24:37 +0800

    feat: display a targeting box on image left click
    The size of the targeting box is relative to the size of the image.

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,64 @@
 /* eslint-disable jsx-a11y/no-noninteractive-element-interactions */
 
+import { useState } from "react";
 import image from "./images/29ya069ug2f61.jpg";
 
 export default function App() {
+  const [dialog, setDialog] = useState({
+    isShown: false,
+    style: {
+      left: 0,
+      top: 0,
+      width: 0,
+      height: 0,
+    },
+  });
+
   const handleMouseDown: React.MouseEventHandler = (event) => {
     if (event.target === event.currentTarget && event.button === 0) {
       const { offsetX, offsetY } = event.nativeEvent;
+      const { clientWidth: imageWidth, clientHeight: imageHeight } =
+        event.currentTarget;
       console.clear();
-      console.log(
-        "X:",
-        offsetX / event.currentTarget.clientWidth,
-        "Y:",
-        offsetY / event.currentTarget.clientHeight
-      );
+      console.log("X:", offsetX / imageWidth, "Y:", offsetY / imageHeight);
+      if (dialog.isShown) {
+        setDialog({
+          ...dialog,
+          isShown: false,
+        });
+      } else {
+        const scaleFactor = 30;
+        setDialog({
+          isShown: true,
+          style: {
+            left: offsetX - imageWidth / (2 * scaleFactor),
+            top: offsetY - imageWidth / (2 * scaleFactor),
+            width: imageWidth / scaleFactor,
+            height: imageWidth / scaleFactor,
+          },
+        });
+      }
     }
   };
 
   return (
     <div>
       <div className="select-none bg-neutral-800 p-4 sm:p-20">
-        <img
-          src={image}
-          alt=""
-          draggable="false"
-          onMouseDown={handleMouseDown}
-          className="mx-auto shadow-lg shadow-neutral-900"
-        />
+        <div className="relative">
+          <img
+            src={image}
+            alt=""
+            draggable="false"
+            onMouseDown={handleMouseDown}
+            className="mx-auto shadow-lg shadow-neutral-900"
+          />
+          {dialog.isShown && (
+            <div
+              style={dialog.style}
+              className="absolute border border-white bg-transparent sm:border-2"
+            />
+          )}
+        </div>
       </div>
       <div className="bg-neutral-900 py-2 text-center text-neutral-400">
         Artwork -{" "}
