d8f75efcaec5715bb9dd27b8a285ae1efc8af626
Dennis Ojeda <royojeda1@gmail.com>
2023-02-24 15:28:07 +0800

    style: deescalate "no-unused-vars" to warnings

--- .eslintrc.js
+++ .eslintrc.js
@@ -19,5 +19,7 @@ module.exports = {
   plugins: ["react"],
   rules: {
     "react/react-in-jsx-scope": "off",
+    "no-unused-vars": "off",
+    "@typescript-eslint/no-unused-vars": "warn"
   },
 };
