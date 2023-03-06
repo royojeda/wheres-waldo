09cd2f33c939d791cc1d68c300e223b9e48244ad
Dennis Ojeda <royojeda1@gmail.com>
2023-03-06 10:49:48 +0800

    feat: format ESLint config with Prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -20,6 +20,6 @@ module.exports = {
   rules: {
     "react/react-in-jsx-scope": "off",
     "no-unused-vars": "off",
-    "@typescript-eslint/no-unused-vars": "warn"
+    "@typescript-eslint/no-unused-vars": "warn",
   },
 };
